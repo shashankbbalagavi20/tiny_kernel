c32780271655fc54c75111766b2dd72766b750e5
shashankBalagavi <shashankbalagavi20@gmail.com>
2025-05-19 22:51:25 +0530

    Implemented scheduler class defination

---
+++ include/scheduler.hpp
@@ -0,0 +1,31 @@
+#ifndef SCHEDULER_HPP
+#define SCHEDULER_HPP
+
+#include <vector>
+#include <queue>
+#include "thread.hpp"
+
+class Scheduler {
+public:
+    Scheduler();
+
+    void add_thread(ThreadControlBlock& thread);
+    ThreadControlBlock* get_next_thread();
+    void remove_thread(ThreadControlBlock* thread);
+
+private:
+    // Ready queue
+    std::priority_queue<ThreadControlBlock*, std::vector<ThreadControlBlock*>, ComparePriority> readyQueue;
+
+    // Currently running thread (nullptr if none)
+    ThreadControlBlock* currentThread;
+
+    // Comparator for the priority queue (higher priority gets higher precedence)
+    struct ComparePriority {
+        bool operator()(const ThreadControlBlock* a, const ThreadControlBlock* b){
+            return a->get_priority() < b->get_priority();
+        }
+    };
+};
+
+#endif // SCHEDULER_HPP
